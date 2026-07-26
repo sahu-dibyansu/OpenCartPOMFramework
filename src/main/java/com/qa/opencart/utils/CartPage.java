fba384b055ed91d3f0977ea9d7699c9f384adde9
sahu-dibyansu <sahu.dibyansuranjan1999@gmail.com>
2026-07-26 08:56:44 +0530

    changes are done as per Naveen's review comments

--- src/main/java/com/qa/opencart/utils/CartPage.java
+++ src/main/java/com/qa/opencart/utils/CartPage.java
@@ -1,11 +1,13 @@
 package com.qa.opencart.utils;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
     int i = 10;
+    public By cartLocator = By.id("cart");
 
     public void cart(){
-
         System.out.println("CartPage Method");
     }
 }
