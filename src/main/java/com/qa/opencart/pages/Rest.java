5d2e75b714e93f992f886cf0e7847516baf11b1c
Dibyansu Ranjan Sahu <66050087+sahu-dibyansu@users.noreply.github.com>
2026-07-26 13:19:51 +0530

    Add Rest class with main method for testing

---
+++ src/main/java/com/qa/opencart/pages/Rest.java
@@ -0,0 +1,8 @@
+package com.qa.opencart.pages;
+
+public class Rest {
+
+    public static void main(String []args){
+        System.out.println("Richa's testing page on the M2.0 branch.");
+    }
+}
