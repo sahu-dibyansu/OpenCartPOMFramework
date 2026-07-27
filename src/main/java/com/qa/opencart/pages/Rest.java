a662ec2d0fa291bf4660261a3f18b34fe22c2697
sahu-dibyansu <sahu.dibyansuranjan1999@gmail.com>
2026-07-27 20:23:04 +0530

    deleted the irrelivant files

--- src/main/java/com/qa/opencart/pages/Rest.java
+++
@@ -1,8 +0,0 @@
-package com.qa.opencart.pages;
-
-public class Rest {
-
-    public static void main(String []args){
-        System.out.println("Richa's testing page on the M2.0 branch.");
-    }
-}
